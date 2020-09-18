5f66c2fc33475d21dfc908701678ae6036e4bb8e
ShilohBeeler <shiloh@ou.edu>
2020-09-18 11:55:00 -0500

    Initial commit with first (lab4) function.

---
+++ R/dotplotwithlm.R
@@ -0,0 +1,16 @@
+#' @title Dot Plot with LM abline
+#'
+#' @param x The x-axis variable
+#' @param inputdata The data frame used for the dotplot and linear model.
+#' @param y The y-axis variable
+#'
+#' @return Outputs a dotplot with the linear model drawn onto it.
+#' @export
+#'
+#' @examples
+#' \dontrun{spruce.df<-read.csv("Spruce.df");dotplotwithlm(inputdata=spruce.df, x=BHDiameter, y=Height)}
+dotplotwithlm <- function(inputdata, x, y) {
+  my.lm = lm(y~x, data=inputdata)
+  with(inputdata, plot(y~x,bg="Blue",pch=21,cex=1.1, ylim=c(0,1.1*max(y)),xlim=c(0,1.1*max(x))))
+  abline(my.lm)
+}
