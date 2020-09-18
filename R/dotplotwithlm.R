87e76d6e99bbab666717ef0344289bb535ed3175
ShilohBeeler <shiloh@ou.edu>
2020-09-18 12:04:50 -0500

    Updated to use data as a parameter.

--- R/dotplotwithlm.R
+++ R/dotplotwithlm.R
@@ -1,7 +1,7 @@
 #' @title Dot Plot with LM abline
 #'
 #' @param x The x-axis variable
-#' @param inputdata The data frame used for the dotplot and linear model.
+#' @param data The data frame used for the dotplot and linear model.
 #' @param y The y-axis variable
 #'
 #' @return Outputs a dotplot with the linear model drawn onto it.
@@ -9,8 +9,8 @@
 #'
 #' @examples
 #' \dontrun{spruce.df<-read.csv("Spruce.df");dotplotwithlm(inputdata=spruce.df, x=BHDiameter, y=Height)}
-dotplotwithlm <- function(inputdata, x, y) {
-  my.lm = lm(y~x, data=inputdata)
-  with(inputdata, plot(y~x,bg="Blue",pch=21,cex=1.1, ylim=c(0,1.1*max(y)),xlim=c(0,1.1*max(x))))
+dotplotwithlm <- function(data, x, y) {
+  my.lm = lm(y~x, data=data)
+  with(data, plot(y~x,bg="Blue",pch=21,cex=1.1, ylim=c(0,1.1*max(y)),xlim=c(0,1.1*max(x))))
   abline(my.lm)
 }
